6b8833f3b29cef739081f31bfc876090cf01ce78
Tito Valiente <titolvaliente@gmail.com>
2017-10-11 15:44:16 -0500

    finished JavaScript w/ HTML exercise

---
+++ js/external.js
@@ -0,0 +1,12 @@
+"use strict";
+
+
+
+var userInput = prompt('What is your favorite color?');
+var favoriteColor = alert('Great, ' + userInput + ' is my favorite color too!');
+
+
+
+alert("Welcome to my Website!");
+console.log("Hello from external JavaScript");
+
