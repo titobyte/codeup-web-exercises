49a1ee85cb9e41aa79b35261083bc21fd4f782e3
Tito Valiente <titolvaliente@gmail.com>
2017-10-12 11:56:22 -0500

    finished basic function writing practice exercises

--- js/functions.js
+++ js/functions.js
@@ -116,17 +116,49 @@ alert('Ok, ' + calculateTip(userInputBill, userInputTip)+ ' should be your tip')
 
 // 1) Make a function called returnTwo() that returns the number 2 when called
 // ---Test this function with console.log(returnTwo())
+
+    function returnTwo() {
+        return 2;
+    }
+
+    console.log(returnTwo());
+
 //
 //     2) Make a function called returnName() that returns the string of your name
 // ---Test this function with console.log(returnName())
+
+    function returnName() {
+        return "Tito";
+    }
+
+    console.log(returnName());
+
 //
 //     3) Make a function called addThree() which takes in a number input and returns the number plus 3. Remember to define a parameter.
 // ---Test this function with console.log(addThree(5))
 //
+    function addThree(num) {
+        return num + 3;
+    }
+
+    console.log(addThree(5));
+
 //     4) Make a function called sayString() which returns the string input passed in.
 // ---Test this function with console.log(sayString())
-//
+
+    function sayString(input) {
+        return input ;
+    }
+
+    console.log(sayString('Hello'));
+
 //     5) Make a function called sayHowdy() which console.logs the string “Howdy!”
 //     ---Test this function by directly calling sayHowdy()
 // ---Remember this function does not need a defined return value
 
+
+    function sayHowdy() {
+        console.log('Howdy!');
+    }
+
+    sayHowdy();
