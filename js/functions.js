125d336417401b0683ab9a5a152ab1086bab3fbc
Tito Valiente <titolvaliente@gmail.com>
2017-10-12 15:44:51 -0500

    finished function drills and super duper gold star function exercises

--- js/functions.js
+++ js/functions.js
@@ -162,3 +162,150 @@ alert('Ok, ' + calculateTip(userInputBill, userInputTip)+ ' should be your tip')
     }
 
     sayHowdy();
+
+// Function Drills
+
+// Write a function called `identity(input)` that takes in an argument called input and returns that input.
+
+    function identity(input) {
+        return input;
+    }
+
+   console.log(identity(9));
+
+
+
+//
+// Write a function called `getRandomNumber(min, max)` that returns a random number
+// between min and max values sent to that function call.
+//
+    function getRandomNumber(min, max) {
+        return (Math.floor( Math.random() *(max - min)) +1  + min);
+    }
+
+
+
+// Write a function called `first(input)` that returns the first character in the provided string.
+
+    function first(input) {
+        return input[0];
+    }
+
+
+//
+//  Write a fuction called `last(input)` that returns the last character of a string
+
+    function last(input) {
+        return input[input.length -1];
+    }
+
+
+
+// Write a function called `rest(input)` that returns everything but the first character of a string.
+
+    function rest(input) {
+        return input.substring(1);
+    }
+
+
+
+
+//     Write a function called `reverse(input)` that takes a string and returns it reversed.
+
+    function reverse(input) {
+        var retVal = '';
+        for (var i = input.length -1; i >= 0; i-- ) {
+            retVal = retVal.concat(input[i]);
+        }
+        return retVal;
+
+    }
+
+//     Write a function called `isNumeric(input)` that takes an input and returns a boolean if the
+// input is numeric.
+
+function isNumeric(input) {
+    return !isNaN(input);
+}
+
+// Write a function called `count(input)` that takes in a string and returns the number of characters.
+
+    function count(input) {
+        return input.length;
+    }
+
+//
+//     Write a function called `add(a, b)` that returns the sum of a and b
+
+    function add(a, b) {
+        return a + b;
+    }
+//
+// Write a function called `subtract(a, b)` that return the difference between the two inputs.
+
+    function subtract(a,b) {
+        return a - b;
+    }
+
+//
+//Write `multiply(a, b)` function that returns the product
+
+    function multiply(a, b) {
+        return a * b;
+    }
+
+//
+// Write a divide(numerator, denominator) function that returns a divided by b
+
+    function divide(numerator, denominator) {
+        return numerator / denominator;
+    }
+
+//
+// Write a remainder(number, divisor) function that returns the remainder left over
+// when dividing `number` by the `divisor`
+
+    function remainder(number, divisor) {
+        return number % divisor;
+    }
+
+//
+// Write the function `square(a)` that takes in a number and returns the number multiplied by itself.
+
+    function square(a) {
+        return a * a;
+    }
+//
+// # Super Duper Gold-Star Bonus
+//
+// Write a function called sumOfSquares(a, b) that uses only your add() function and your
+// square function and not + or * operators
+
+    function sumOfSquare(a , b) {
+           return add(square(a),square(b));
+    }
+
+
+
+// Write a function called doMath(operator, a, b) that takes 3 parameters.
+// The first parameter is the name of the math function you want to apply.
+// a and b are the two numbers to run that function on.
+
+
+    function doMath(operator, a, b) {
+        switch (operator) {
+            case "add":
+                return add(a,b);
+                 break;
+            case "subtract":
+                return subtract(a, b);
+                break;
+            case "multiply":
+                return multiply(a, b);
+                 break;
+            default:
+                alert("not a valid operator");
+                break;
+        }
+
+    }
