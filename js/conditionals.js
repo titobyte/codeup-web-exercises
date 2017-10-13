a930990792379893be23029a12746a4223f8e571
Tito Valiente <titolvaliente@gmail.com>
2017-10-13 15:32:30 -0500

    finished conditional exercise

--- js/conditionals.js
+++ js/conditionals.js
@@ -18,23 +18,46 @@
 
 /* ########################################################################## */
 
+    // functions
+
+    function isEven(num) {
+        if (num % 2 === 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+
+    function pHundred(num) {
+        return parseInt(num) + 100;
+    }
+
+    function numPosOrNeg(num){
+        return num >= 0;
+    }
+
+    function notNum(num) {
+        if (isNaN(num)) {
+             alert('This is not a number');
+        } else {
+             alert("This is a number");
+        }
+    }
+
+
     if (window.confirm("Would you like to enter a number?")) {
 
-        prompt("Can you enter a number?");
+        var numPlaceholder = prompt("Can you enter a number?");
+        alert("That number is " + isEven(numPlaceholder));
+        alert("Plus one hundred " + pHundred(numPlaceholder));
+        alert("that number is " + numPosOrNeg(numPlaceholder));
+        notNum(numPlaceholder);
 
-    }   else if (alert('that number is even')) {
-
-    }   else if (alert('that number is odd')) {
-
-    }
+        } else {
+            alert("Bye!");
+        }
 
 
-
-
-
-    else  {
-        alert("Bye!");
-    }
 
 
 /**
@@ -55,6 +78,24 @@
  * console.logging the function's return value
  */
 
+
+    function analyzeColor(colorStr) {
+        if (colorStr === 'blue'){
+            return "blue is the color of the sky";
+        } else if (colorStr === 'red') {
+            return "Strawberries are red"
+        } else  if (colorStr === 'yellow') {
+            return "The sun is yellow";
+        } else  if (colorStr === 'green') {
+            return "the grass is green";
+        } else  if (colorStr === 'indigo') {
+            return "indigo is weird";
+        }   else {
+            return "violet is my favorite color";
+        }
+    }
+    console.log(analyzeColor());
+
 // Don't change the next two lines!
 // These lines create two variables for you:
 // - `colors`: a list of the colors of the rainbow
@@ -68,10 +109,41 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * You should see a different message everytime you refresh the page
  */
 
+    console.log(analyzeColor(randomColor));
+
 /**
  * TODO:
  * Refactor your above function to use a switch-case statement
  */
+
+
+function analyzeColorSwitch() {
+
+
+    switch(randomColor) {
+        case "blue":
+            return  "blue is the color of the sky";
+            break;
+        case "red":
+            return "Strawberries are red";
+            break;
+        case "yellow":
+            return"The sun is yellow";
+            break;
+        case "green":
+            return"the grass is green";
+            break;
+        case "indigo":
+            return "indigo is werid";
+            break;
+        default:
+            return"violet is my favorite color";
+            break;
+    }
+}
+
+console.log(analyzeColorSwitch());
+
 
 /**
  * TODO:
@@ -79,6 +151,13 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * user to your `analyzeColor` function. Alert the return value from your
  * function to show it to the user.
  */
+
+
+
+    var colorInput = prompt("Can I get a color?");
+    alert(analyzeColor(colorInput));
+
+
 
 /* ########################################################################## */
 
@@ -102,6 +181,40 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * return value.
  */
 
+
+    var luckNum = Math.floor(Math.random() * 5);
+
+    function caculateTotal(luckNum, totalAmount) {
+
+        if (luckNum === 0) {
+
+            return totalAmount;
+
+        } else if (luckNum === 1) {
+
+           return totalAmount * .9;
+
+        } else  if (luckNum === 2) {
+
+            return totalAmount * .8;
+
+        } else  if (luckNum === 3) {
+
+            return totalAmount * .7;
+
+        } else  if (luckNum === 4) {
+
+            return totalAmount * .5;
+
+        } else  {
+
+            return 0;
+        }
+
+    }
+
+    console.log(caculateTotal(luckNum, 100));
+
 /**
  * TODO:
  * Uncomment the line below to generate a random number between 0 and 6.
@@ -109,5 +222,14 @@ var randomColor = colors[Math.floor(Math.random() * colors.length)];
  * and alerts to display to the user what their lucky number was, what their
  * price before the discount was, and what their price after the discount is.
  */
-// Generate a random number between 0 and 6
-// var luckyNumber = Math.floor(Math.random() * 6);
+    // Generate a random number between 0 and 6
+    var luckyNumber = Math.floor(Math.random() * 6) + 1;
+
+
+    var totalAmount = parseInt(prompt("What is your total bill?"));
+
+    alert("Your lucky number is " + luckyNumber);
+    alert("Your total is " +  totalAmount);
+    alert("Your discount is " + caculateTotal(luckyNumber, totalAmount));
+
+
