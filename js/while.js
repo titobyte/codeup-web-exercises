1bd86621f9fc24fe5490bc023e6af191291772c4
Tito Valiente <titolvaliente@gmail.com>
2017-10-14 15:53:43 -0500

    stuck on exercise two

--- js/while.js
+++ js/while.js
@@ -1,8 +1,32 @@
 "use strict";
 
+// Basic while loop
 
-var i = 0;
-while (i < 10) {
-    console.log('while loop iteration #' + i);
-    i++;
+function basicWhileLoop() {
+    var i = 1;
+    while (i < 65536) {
+        i = i * 2;
+        console.log(i);
+
+    }
 }
+
+// Ice cream loop
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+
+
+function conesSold() {
+
+    var i = 1;
+
+    do  {
+        console.log(allCones);
+        console.log(Math.floor(Math.random() * 5) + 1);
+        i++;
+
+    } while (i < 6);
+
+}
+console.log(conesSold()); 
