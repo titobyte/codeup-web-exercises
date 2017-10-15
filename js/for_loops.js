79c7d4b01605743e6154ca0a16308395db200562
Tito Valiente <titolvaliente@gmail.com>
2017-10-15 17:42:12 -0500

    finished loop exercise 5

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,42 +1,7 @@
 "use strict";
 
 
-function showMultiplicationTable() {
-
-}
-
-function showMultiplicationTable() {
-
-}
-
-function showMultiplicationTable() {
-
-}
-function showMultiplicationTable() {
-
-}
-function showMultiplicationTable() {
-
-}
-function showMultiplicationTable() {
-
-}
-
-function showMultiplicationTable(seven) {
-
-
-   return seven * 1 = 7
-    7 x 2 = 14
-    7 x 3 = 21
-    7 x 4 = 28
-    7 x 5 = 35
-    7 x 6 = 42
-    7 x 7 = 49
-    7 x 8 = 56
-    7 x 9 = 63
-    7 x 10 = 70
-
-}
+// For loop exercises
 
 function showMultiplicationTable() {
 
@@ -49,3 +14,56 @@ function showMultiplicationTable() {
 function showMultiplicationTable() {
 
 }
+function showMultiplicationTable() {
+
+}
+function showMultiplicationTable() {
+
+}
+function showMultiplicationTable() {
+
+}
+
+function showMultiplicationTable(num) {
+
+
+   if (num === 7) {
+       console.log('7 x 1 = 7\n' +
+           '7 x 2 = 14\n' +
+           '7 x 3 = 21\n' +
+           '7 x 4 = 28\n' +
+           '7 x 5 = 35\n' +
+           '7 x 6 = 42\n' +
+           '7 x 7 = 49\n' +
+           '7 x 8 = 56\n' +
+           '7 x 9 = 63\n' +
+           '7 x 10 = 70');
+
+   }
+
+}
+
+showMultiplicationTable()
+
+function showMultiplicationTable() {
+
+}
+
+function showMultiplicationTable() {
+
+}
+
+function showMultiplicationTable() {
+
+}
+
+
+
+
+// Exercise number 5
+
+for (var i = 100; i >= 5; i = i -5) {
+    console.log(i);
+}
+
+
