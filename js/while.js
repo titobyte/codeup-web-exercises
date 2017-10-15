e64cfc02badfab80d66cfef5b8fb4eadb8192576
Tito Valiente <titolvaliente@gmail.com>
2017-10-15 16:01:39 -0500

    starting for loop exercise

--- js/while.js
+++ js/while.js
@@ -11,22 +11,43 @@ function basicWhileLoop() {
     }
 }
 
+// console.log(basicWhileLoop());
+
+
 // Ice cream loop
 // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
 // This expression will generate a random number between 1 and 5
-
-
-function conesSold() {
-
-    var i = 1;
-
-    do  {
-        console.log(allCones);
-        console.log(Math.floor(Math.random() * 5) + 1);
-        i++;
-
-    } while (i < 6);
-
-}
-console.log(conesSold()); 
+// var randomCone = Math.floor(Math.random() * 5) + 1;
+//
+//  var coneVal = function conesSold() {
+//
+//         var i = 1;
+//
+//         do  {
+//             console.log(allCones);
+//             console.log(randomCone);
+//             i++;
+//
+//         } while (i < 1);
+//
+//
+//
+//      if (randomCone === 1) {
+//          return "one cone sold";
+//      } else if (randomCone === 2){
+//          return "two cones sold";
+//      }  else if (randomCone === 3) {
+//          return "three cones sold";
+//      }
+//
+//
+//
+//  }
+//
+//
+//
+//
+//
+// coneVal();
+//
