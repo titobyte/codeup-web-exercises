a7c8e501145d79ea0b32653847c6ee7df4db0db1
Tito Valiente <titolvaliente@gmail.com>
2017-10-15 18:04:11 -0500

    finished break and continue exercise

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,3 +1,22 @@
 "use strict";
 
-console.log('test');
+    var random = Math.floor(Math.random()*50 +1);
+
+    console.log('Number to skip is : ' + random);
+
+
+    for (var i = 1; i < 100; i++) {
+
+        if (i % 2 === 0) {
+            continue;
+        }
+
+        if (random === i){
+            console.log('Yikes! Skipping number: ' + i);
+        }
+        console.log ("Here is an odd number: " + i);
+
+        if (i >= 49) {
+            break;
+        }
+    }
