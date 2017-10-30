41dac5c7f3b9dd9fd5167a4a92c689b838d07efd
Tito Valiente <titolvaliente@gmail.com>
2017-10-30 12:14:16 -0500

    finished class selectors on jquery exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
 "use strict";
 
-var alertHeaderContent = $('#id-header').html();
-
-alert(alertHeaderContent);
-
-
-var alertParagraphContent = $('#id-para').html();
-
-alert(alertParagraphContent);
-
+$('.codeup').css('border', '1px solid red');
 });
