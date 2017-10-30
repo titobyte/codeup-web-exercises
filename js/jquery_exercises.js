0c210bbd5452155eb9aa7a3c1082744715c9965d
Tito Valiente <titolvaliente@gmail.com>
2017-10-30 12:07:34 -0500

    finished id selectors for jquery exercise

---
+++ js/jquery_exercises.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+"use strict";
+
+var alertHeaderContent = $('#id-header').html();
+
+alert(alertHeaderContent);
+
+
+var alertParagraphContent = $('#id-para').html();
+
+alert(alertParagraphContent);
+
+});
