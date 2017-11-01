a297795770af5d76eb6afd489947216907110a8d
Tito Valiente <titolvaliente@gmail.com>
2017-11-01 10:46:43 -0500

    starting traversing exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
 "use strict";
 
-    $('#toggle').click(function (e) {
-        e.preventDefault();
-        $('dd').toggleClass('invisible')
-    })
-
+    // $('#toggle').click(function (e) {
+    //     e.preventDefault();
+    //     $('dd').toggleClass('invisible')
+    // })
+    //
+    $('li').css('background-color', '#FF0');
+    $('li').parent().css('background-color', '#0F0');
 
 });
