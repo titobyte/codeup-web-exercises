470626afeb9b9464136907df62ef14dfd1a21fa9
Tito Valiente <titolvaliente@gmail.com>
2017-11-01 12:09:39 -0500

    finished jQuery effect exercise

--- js/jquery_faq.js
+++ js/jquery_faq.js
@@ -6,7 +6,30 @@ $(document).ready(function () {
     //     $('dd').toggleClass('invisible')
     // })
     //
-    $('li').css('background-color', '#FF0');
-    $('li').parent().css('background-color', '#0F0');
+    // $('li').css('background-color', '#FF0');
+    // $('li').parent().css('background-color', '#0F0');
+
+
+    //Effects Exercises
+    // # 1
+    $('#toggleHeader').click(function () {
+       $('#toggleHeader').parent().hide();
+
+    });
+
+
+    // # 2
+    $('ul').hide();
+    $('h3').click(function () {
+        $('ul').slideToggle().show();
+    });
+
+
+    //# 3
+
+    $('h2').hide().delay( 8000 ).fadeIn();
+
+
+
 
 });
