b5116eb2cfad01d329e8112619212e2b6f5fd425
Tito Valiente <titolvaliente@gmail.com>
2017-11-08 15:28:35 -0600

    finished es6 exercise

--- js/es6.js
+++ js/es6.js
@@ -55,37 +55,50 @@ let emails = [];
 let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach(function(user) {
-  return emails.push(user.email);
-});
-users.forEach(function(user) {
-  return names.push(user.name);
-});
+// users.forEach(function(user) {
+//   return emails.push(user.email);
+// });
+// users.forEach(function(user) {
+//   return names.push(user.name);
+// });
 
 //ES6
-
+//If one parameter.
+users.forEach(user => {
+  return emails.push(user.email);
+});
+users.forEach((user) => {
+    return names.push(user.name);
+});
 
 // TODO: replace `var` with `let` in the following declaration
 let developers = [];
-users.forEach(function(user) {
+users.forEach(function({name, email, languages}) {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
   //       parameter definition
-  const name = user.name;
-  const email = user.email;
-  const languages = user.languages;
+
 
   // TODO: rewrite the assignment below to use template strings
-  developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
-});
+//   developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+// });
+
+    developers.push(` ${name}'s email is ${email} ${name} knows ${languages.join(', ')}. `)});
+
 
 // TODO: Use `let` for the following variable
-var list = '<ul>';
+let list = '<ul>';
 
-// TODO: rewrite the following loop to use a for..of loop
-developers.forEach(function (developer) {
+// // TODO: rewrite the following loop to use a for..of loop
+// developers.forEach(function (developer) {
+//
+//   // TODO: rewrite the assignment below to use template strings
+//   list += '<li>' + developer + '</li>';
+// });
+// list += '</ul>';
 
-  // TODO: rewrite the assignment below to use template strings
-  list += '<li>' + developer + '</li>';
-});
-list += '</ul>';
+  for (let developer of developers) {
+      list += '<li>' + developer + '</li>';
+  }
+    list += '</ul>';
+  document.body.innerHTML = list;
