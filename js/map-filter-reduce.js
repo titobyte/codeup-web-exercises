3c98a2a09c22378519b19c2bd2d95a903aeaea29
Tito Valiente <titolvaliente@gmail.com>
2017-11-09 14:12:55 -0600

    es6 map filter reduce exercise

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,52 @@
+"use strict";
+
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+    },
+];
+
+
+const language = users.filter(user => {
+    return user.languages.length >= 3;
+});
+
+console.log(language);
+
+let usersEmail = users.map(email => email.email);
+
+console.log(usersEmail);
+
+const idUser = users.reduce((object, user) => {
+    object[user.id] = user;
+    return object;
+}, {});
+
+console.log(idUser);
