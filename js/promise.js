96eab16348f0296a60f3e4fe636aa1236b0d397e
Tito Valiente <titolvaliente@gmail.com>
2017-11-10 13:46:16 -0600

    promise exercise update

--- js/promise.js
+++ js/promise.js
@@ -1,2 +1,12 @@
 "use strict";
 
+
+    const wait = delay => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+          }, delay);
+      });
+    };
+wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
